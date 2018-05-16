df3d9334650ea3a4df1a102d5c3a3b9673fc24c5
Ken Kuang <sykuang.tw@gmail.com>
2018-05-16 17:01:18 +0800

    update 659.split-array-into-consecutive-subsequences.py

---
+++ 659.split-array-into-consecutive-subsequences.py
@@ -0,0 +1,77 @@
+#
+# [659] Split Array into Consecutive Subsequences
+#
+# https://leetcode.com/problems/split-array-into-consecutive-subsequences/description/
+#
+# algorithms
+# Medium (36.93%)
+# Total Accepted:    8.7K
+# Total Submissions: 23.5K
+# Testcase Example:  '[1,2,3,3,4,5]'
+#
+# You are given an integer array sorted in ascending order (may contain
+# duplicates), you need to split them into several subsequences, where each
+# subsequences consist of at least 3 consecutive integers. Return whether you
+# can make such a split.
+#
+# Example 1:
+#
+# Input: [1,2,3,3,4,5]
+# Output: True
+# Explanation:
+# You can split them into two consecutive subsequences :
+# 1, 2, 3
+# 3, 4, 5
+#
+#
+#
+# Example 2:
+#
+# Input: [1,2,3,3,4,4,5,5]
+# Output: True
+# Explanation:
+# You can split them into two consecutive subsequences :
+# 1, 2, 3, 4, 5
+# 3, 4, 5
+#
+#
+#
+# Example 3:
+#
+# Input: [1,2,3,4,4,5]
+# Output: False
+#
+#
+#
+# Note:
+#
+# The length of the input is in range of [1, 10000]
+#
+#
+#
+
+
+class Solution(object):
+    def isPossible(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        feq = collections.defaultdict(int)
+        need = collections.defaultdict(int)
+        for n in nums:
+            feq[n] += 1
+        for n in nums:
+            if feq[n]==0:
+                continue
+            elif need[n] > 0:
+                need[n] -= 1
+                need[n+1] += 1
+            elif feq[n+1]>0 and feq[n+2]>0:
+                feq[n+1] -= 1
+                feq[n+2] -= 1
+                need[n+3] += 1
+            else:
+                return False
+            feq[n]-=1
+        return True
