d25c1c5cc191ba42328fd9132e8aa0b705b5aa1e
Ken Kuang <sykuang.tw@gmail.com>
2018-05-22 10:44:21 +0800

    update 10.regular-expression-matching.py

---
+++ 10.regular-expression-matching.py
@@ -0,0 +1,105 @@
+#
+# [10] Regular Expression Matching
+#
+# https://leetcode.com/problems/regular-expression-matching/description/
+#
+# algorithms
+# Hard (24.34%)
+# Total Accepted:    200.7K
+# Total Submissions: 824.6K
+# Testcase Example:  '"aa"\n"a"'
+#
+# Given an input string (s) and a pattern (p), implement regular expression
+# matching with support for '.' and '*'.
+#
+#
+# '.' Matches any single character.
+# '*' Matches zero or more of the preceding element.
+#
+#
+# The matching should cover the entire input string (not partial).
+#
+# Note:
+#
+#
+# s could be empty and contains only lowercase letters a-z.
+# p could be empty and contains only lowercase letters a-z, and characters like
+# . or *.
+#
+#
+# Example 1:
+#
+#
+# Input:
+# s = "aa"
+# p = "a"
+# Output: false
+# Explanation: "a" does not match the entire string "aa".
+#
+#
+# Example 2:
+#
+#
+# Input:
+# s = "aa"
+# p = "a*"
+# Output: true
+# Explanation: '*' means zero or more of the precedeng element, 'a'. Therefore,
+# by repeating 'a' once, it becomes "aa".
+#
+#
+# Example 3:
+#
+#
+# Input:
+# s = "ab"
+# p = ".*"
+# Output: true
+# Explanation: ".*" means "zero or more (*) of any character (.)".
+#
+#
+# Example 4:
+#
+#
+# Input:
+# s = "aab"
+# p = "c*a*b"
+# Output: true
+# Explanation: c can be repeated 0 times, a can be repeated 1 time. Therefore
+# it matches "aab".
+#
+#
+# Example 5:
+#
+#
+# Input:
+# s = "mississippi"
+# p = "mis*is*p*."
+# Output: false
+#
+#
+#
+
+
+class Solution(object):
+    def isMatch(self, s, p):
+        """
+        :type s: str
+        :type p: str
+        :rtype: bool
+        """
+        if not p:
+            return not s
+        if len(p) == 1:
+            return len(s) == 1 and (s == p or p == ".")
+        if p[1] != "*":
+            if not s:
+                return False
+            if p[0] == "." or p[0] == s[0]:
+                return self.isMatch(s[1:], p[1:])
+            return False
+        while s and (p[0] == s[0] or p[0] == "."):
+            if self.isMatch(s, p[2:]):
+                return True
+            s = s[1:]
+        return self.isMatch(s, p[2:])
