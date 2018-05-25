2131cdd775b8f8837e75da60338c499b8efce6c4
Ken Kuang <sykuang.tw@gmail.com>
2018-05-25 15:02:55 +0800

    update 284.peeking-iterator.py

---
+++ 284.peeking-iterator.py
@@ -0,0 +1,95 @@
+#
+# [284] Peeking Iterator
+#
+# https://leetcode.com/problems/peeking-iterator/description/
+#
+# algorithms
+# Medium (35.50%)
+# Total Accepted:    56.2K
+# Total Submissions: 158.2K
+# Testcase Example:  '[1,2,3,4]\n[0,1,1,2,2,1,1,2,0,1,0,2,0]'
+#
+# Given an Iterator class interface with methods: next() and hasNext(), design
+# and implement a PeekingIterator that support the peek() operation -- it
+# essentially peek() at the element that will be returned by the next call to
+# next().
+#
+# Example:
+#
+#
+# Assume that the iterator is initialized to the beginning of the list:
+# [1,2,3].
+#
+# Call next() gets you 1, the first element in the list.
+# Now you call peek() and it returns 2, the next element. Calling next() after
+# that still return 2.
+# You call next() the final time and it returns 3, the last element.
+# Calling hasNext() after that should return false.
+#
+#
+# Follow up: How would you extend your design to be generic and work with all
+# types, not just integer?
+#
+#
+# Below is the interface for Iterator, which is already defined for you.
+#
+# class Iterator(object):
+#     def __init__(self, nums):
+#         """
+#         Initializes an iterator object to the beginning of a list.
+#         :type nums: List[int]
+#         """
+#
+#     def hasNext(self):
+#         """
+#         Returns true if the iteration has more elements.
+#         :rtype: bool
+#         """
+#
+#     def next(self):
+#         """
+#         Returns the next element in the iteration.
+#         :rtype: int
+#         """
+
+
+class PeekingIterator(object):
+    def __init__(self, iterator):
+        """
+        Initialize your data structure here.
+        :type iterator: Iterator
+        """
+        self.it = iterator
+        self.peak = []
+
+    def peek(self):
+        """
+        Returns the next element in the iteration without advancing the iterator.
+        :rtype: int
+        """
+        if not self.peak:
+            self.peak.append(self.it.next())
+        return self.peak[0]
+
+    def next(self):
+        """
+        :rtype: int
+        """
+        if self.peak:
+            return self.peak.pop()
+        return self.it.next()
+
+    def hasNext(self):
+        """
+        :rtype: bool
+        """
+        if self.peak or self.it.hasNext():
+            return True
+        else:
+            return False
+
+# Your PeekingIterator object will be instantiated and called as such:
+# iter = PeekingIterator(Iterator(nums))
+# while iter.hasNext():
+#     val = iter.peek()   # Get the next element but not advance the iterator.
+#     iter.next()         # Should return the same value as [val].
