03ec47da19289563b97157cec06a3f41a4df5318
Ken Kuang <sykuang.tw@gmail.com>
2018-06-01 14:25:21 +0800

    update 374.guess-number-higher-or-lower.py

---
+++ 374.guess-number-higher-or-lower.py
@@ -0,0 +1,57 @@
+#
+# [374] Guess Number Higher or Lower
+#
+# https://leetcode.com/problems/guess-number-higher-or-lower/description/
+#
+# algorithms
+# Easy (36.99%)
+# Total Accepted:    75.8K
+# Total Submissions: 204.7K
+# Testcase Example:  '10\n6'
+#
+# We are playing the Guess Game. The game is as follows:
+#
+# I pick a number from 1 to n. You have to guess which number I picked.
+#
+# Every time you guess wrong, I'll tell you whether the number is higher or
+# lower.
+#
+# You call a pre-defined API guess(int num) which returns 3 possible results
+# (-1, 1, or 0):
+#
+# -1 : My number is lower
+# ⁠1 : My number is higher
+# ⁠0 : Congrats! You got it!
+#
+#
+# Example:
+#
+# n = 10, I pick 6.
+#
+# Return 6.
+#
+#
+#
+# The guess API is already defined for you.
+# @param num, your guess
+# @return -1 if my number is lower, 1 if my number is higher, otherwise return 0
+# def guess(num):
+
+
+class Solution(object):
+    def guessNumber(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        lower = 1
+        bigger = n
+        while lower <= bigger:
+            res = (lower+bigger)//2
+            r = guess(res)
+            if r > 0:
+                lower = res+1
+            elif r < 0:
+                bigger = res-1
+            else:
+                return res
