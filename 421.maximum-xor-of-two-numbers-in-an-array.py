c28e2c6d95e9541ed4bb98748ab2cf8a4750442b
Ken Kuang <sykuang.tw@gmail.com>
2018-06-01 11:33:27 +0800

    update 421.maximum-xor-of-two-numbers-in-an-array.py

--- 421.maximum-xor-of-two-numbers-in-an-array.py
+++ 421.maximum-xor-of-two-numbers-in-an-array.py
@@ -38,12 +38,14 @@ class Solution(object):
         mask = 0
         for i in xrange(31, -1, -1):
             mask |= 1 << i
+            tmp = res | 1 << i
             s = set()
+            find = False
             for n in nums:
                 s.add(n & mask)
-            tmp = res | 1 << i
-            for n in s:
-                if n ^ tmp in s:
-                    res = tmp
+                if (n ^ tmp) & mask in s:
+                    find = True
                     break
+            if find:
+                res = tmp
         return res
