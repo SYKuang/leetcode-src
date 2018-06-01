773844f5a4551fe29d6cdbb161d133adab062530
Ken Kuang <sykuang.tw@gmail.com>
2018-06-01 17:20:03 +0800

    update 17.letter-combinations-of-a-phone-number.py

--- 17.letter-combinations-of-a-phone-number.py
+++ 17.letter-combinations-of-a-phone-number.py
@@ -40,15 +40,15 @@ class Solution(object):
         """
         if not digits:
             return []
-        table = {"2": ["a", "b", "c"], "3": ["d", "e", "f"], "4": ["g", "h", "i"], "5": ["j", "k", "l"], "6": [
-            "m", "n", "o"], "7": ["p", "q", "r", "s"], "8": ["t", "u", "v"], "9": ["w", "x", "y", "z"]}
+        table = [[], [], ["a", "b", "c"],  ["d", "e", "f"],  ["g", "h", "i"], ["j", "k", "l"],  [
+            "m", "n", "o"],  ["p", "q", "r", "s"],  ["t", "u", "v"],  ["w", "x", "y", "z"]]
         if len(digits) == 1:
-            return table[digits]
+            return table[int(digits)]
         res = [""]
         for d in digits:
             tmp = []
             for pre in res:
-                for post in table[d]:
+                for post in table[int(d)]:
                     tmp.append(pre+post)
             res = tmp
         return res
