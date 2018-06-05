81426a81ffec6082546acf0e3f8aba9c5bcee27f
Ken Kuang <sykuang.tw@gmail.com>
2018-06-05 14:17:00 +0800

    update 658.find-k-closest-elements.py

---
+++ 658.find-k-closest-elements.py
@@ -0,0 +1,78 @@
+#
+# [658] Find K Closest Elements
+#
+# https://leetcode.com/problems/find-k-closest-elements/description/
+#
+# algorithms
+# Medium (34.78%)
+# Total Accepted:    19.6K
+# Total Submissions: 56.5K
+# Testcase Example:  '[1,2,3,4,5]\n4\n3'
+#
+#
+# Given a sorted array, two integers k and x, find the k closest elements to x
+# in the array.  The result should also be sorted in ascending order.
+# If there is a tie,  the smaller elements are always preferred.
+#
+#
+# Example 1:
+#
+# Input: [1,2,3,4,5], k=4, x=3
+# Output: [1,2,3,4]
+#
+#
+#
+#
+# Example 2:
+#
+# Input: [1,2,3,4,5], k=4, x=-1
+# Output: [1,2,3,4]
+#
+#
+#
+# Note:
+#
+# The value k is positive and will always be smaller than the length of the
+# sorted array.
+# ⁠Length of the given array is positive and will not exceed 104
+# ⁠Absolute value of elements in the array and x will not exceed 104
+#
+#
+#
+#
+#
+#
+# UPDATE (2017/9/19):
+# The arr parameter had been changed to an array of integers (instead of a list
+# of integers). Please reload the code definition to get the latest changes.
+#
+#
+
+
+class Solution(object):
+    def findClosestElements(self, arr, k, x):
+        """
+        :type arr: List[int]
+        :type k: int
+        :type x: int
+        :rtype: List[int]
+        """
+        l = bisect.bisect_left(arr, x)
+        if l == 0:
+            return arr[:k]
+        elif l == len(arr)-1:
+            return arr[l-k+1:]
+        r = l+1
+        count = 0
+        res = []
+        arr.insert(0, sys.maxint)
+        arr.append(sys.maxint)
+        while count < k:
+            if abs(x-arr[l]) > abs(x-arr[r]):
+                res.append(arr[r])
+                r += 1
+            else:
+                res.append(arr[l])
+                l -= 1
+            count+=1
+        return sorted(res)
