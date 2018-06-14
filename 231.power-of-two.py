6b5d798a16ee9419ff2a2ce37bd33f067326a36e
Ken Kuang <sykuang.tw@gmail.com>
2018-06-14 14:45:59 +0800

    update 231.power-of-two.py

---
+++ 231.power-of-two.py
@@ -0,0 +1,51 @@
+#
+# [231] Power of Two
+#
+# https://leetcode.com/problems/power-of-two/description/
+#
+# algorithms
+# Easy (40.88%)
+# Total Accepted:    176.7K
+# Total Submissions: 432K
+# Testcase Example:  '1'
+#
+# Given an integer, write a function to determine if it is a power of two.
+#
+# Example 1:
+#
+#
+# Input: 1
+# Output: true
+# Explanation: 20 = 1
+#
+#
+# Example 2:
+#
+#
+# Input: 16
+# Output: true
+# Explanation: 24 = 16
+#
+# Example 3:
+#
+#
+# Input: 218
+# Output: false
+#
+#
+
+
+class Solution(object):
+    def isPowerOfTwo(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        if n <= 0:
+            return False
+        cnt = 0
+        while n:
+            if n & 1:
+                cnt += 1
+            n >>= 1
+        return cnt <= 1
