d03ba6da20291d4d39c4f958f29b65827146dbb6
Ken Kuang <sykuang.tw@gmail.com>
2018-06-26 16:12:16 +0800

    update 228.summary-ranges.py

---
+++ 228.summary-ranges.py
@@ -0,0 +1,54 @@
+#
+# [228] Summary Ranges
+#
+# https://leetcode.com/problems/summary-ranges/description/
+#
+# algorithms
+# Medium (32.73%)
+# Total Accepted:    103.2K
+# Total Submissions: 314.8K
+# Testcase Example:  '[0,1,2,4,5,7]'
+#
+# Given a sorted integer array without duplicates, return the summary of its
+# ranges.
+#
+# Example 1:
+#
+#
+# Input:  [0,1,2,4,5,7]
+# Output: ["0->2","4->5","7"]
+# Explanation: 0,1,2 form a continuous range; 4,5 form a continuous range.
+#
+#
+# Example 2:
+#
+#
+# Input:  [0,2,3,4,6,8,9]
+# Output: ["0","2->4","6","8->9"]
+# Explanation: 2,3,4 form a continuous range; 8,9 form a continuous range.
+#
+#
+#
+
+
+class Solution(object):
+    def summaryRanges(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[str]
+        """
+        if not nums:
+            return []
+        nums.append(nums[0]-1)
+        pre = nums[0]
+        i = 1
+        res = []
+        while i < len(nums):
+            if nums[i] != nums[i-1]+1:
+                if nums[i-1] == pre:
+                    res.append(str(nums[i-1]))
+                else:
+                    res.append("%d->%d" % (pre, nums[i-1]))
+                pre = nums[i]
+            i += 1
+        return res
